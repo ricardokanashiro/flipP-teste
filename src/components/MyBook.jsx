c03d155beb0054d8d5f7fa61fa88f54b04f6d09f
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-05-27 18:02:21 -0300

    adjusting book 1

--- src/components/MyBook.jsx
+++ src/components/MyBook.jsx
@@ -4,11 +4,17 @@ import './MyBook.css'
 
 function MyBook(props) {
   return (
-    <HTMLFlipBook width={400} height={500} usePortrait={true} showCover={true}>
-      <div className="demoPage"></div>
-      <div className="demoPage"></div>
-      <div className="demoPage"></div>
-      <div className="demoPage"></div>
+    <HTMLFlipBook width={420} height={600} usePortrait={true} showCover={true}>
+      <div className="demoPage capa"></div>
+      <div className="demoPage contracapa"></div>
+      <div className="demoPage page1"></div>
+      <div className="demoPage page2"></div>
+      <div className="demoPage page3"></div>
+      <div className="demoPage page4"></div>
+      <div className="demoPage page5"></div>
+      <div className="demoPage page6"></div>
+      <div className="demoPage contracapa"></div>
+      <div className="demoPage capa-final"></div>
     </HTMLFlipBook>
 
     // <HTMLFlipBook width={300} height={500} className="book">
