eefd4bdfa5f7d2a4572f8fa22a143028256edcaf
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-06-04 15:35:08 -0300

    ajustando book1

--- src/components/MyBook.jsx
+++ src/components/MyBook.jsx
@@ -5,29 +5,31 @@ import './MyBook.css'
 import CameraIcon from "../assets/camera-icon.png"
 
 function MyBook(props) {
-  return (
-    <HTMLFlipBook width={400} height={580} usePortrait={true} showCover={true} className="book">
-      <div className="demoPage capa"></div>
-      <div className="demoPage page1"></div>
-      <div className="demoPage page2"></div>
-      <div className="demoPage page3"></div>
-      <div className="demoPage page4"></div>
-      <div className="demoPage page5"></div>
-      <div className="demoPage page6">
-        <a href="https://github.com/ricardokanashiro" target="_blank">
-          <img src={CameraIcon} className="link" />
-        </a>
-      </div>
-      <div className="demoPage capa-final"></div>
-    </HTMLFlipBook>
+   return (
+      <section className="book-wrapper">
+         <HTMLFlipBook width={400} height={580} size={"stretch"} usePortrait={true} showCover={true} className="book">
+            <div className="demoPage capa"></div>
+            <div className="demoPage page1"></div>
+            <div className="demoPage page2"></div>
+            <div className="demoPage page3"></div>
+            <div className="demoPage page4"></div>
+            <div className="demoPage page5"></div>
+            <div className="demoPage page6">
+               <a href="https://github.com/ricardokanashiro" target="_blank">
+                  <img src={CameraIcon} className="link" />
+               </a>
+            </div>
+            <div className="demoPage capa-final"></div>
+         </HTMLFlipBook>
+      </section>
 
-    // <HTMLFlipBook width={300} height={500} className="book">
-    //   <div className="demoPage"></div>
-    //   <div className="demoPage"></div>
-    //   <div className="demoPage"></div>
-    //   <div className="demoPage"></div>
-    // </HTMLFlipBook>
-  );
+      // <HTMLFlipBook width={300} height={500} className="book">
+      //   <div className="demoPage"></div>
+      //   <div className="demoPage"></div>
+      //   <div className="demoPage"></div>
+      //   <div className="demoPage"></div>
+      // </HTMLFlipBook>
+   );
 }
 
 export default MyBook
